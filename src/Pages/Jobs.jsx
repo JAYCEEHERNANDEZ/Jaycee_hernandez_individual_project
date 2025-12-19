8f3e827491186bd0df21237b0ba9dcd67180b9ea
JAYCEEHERNANDEZ <zednanreh1423@gmail.com>
2025-12-19 15:45:16 +0800

    jc

--- src/Pages/Jobs.jsx
+++ src/Pages/Jobs.jsx
@@ -64,7 +64,11 @@ function JobCard({ job }) {
   return (
     <div className="group relative bg-white border border-blue-200 rounded-3xl p-6 shadow-md hover:shadow-lg hover:-translate-y-1 transition-all duration-300 overflow-hidden">
       {/* Basic info */}
-      <h3 className="text-xl font-semibold text-gray-900">{job.title}</h3>
+       <Link
+            to={`/jobs/${job.id}`}
+          >
+            <h3 className="text-xl font-semibold text-gray-900">{job.title}</h3>
+          </Link>
       <p className="text-sm text-gray-600 mt-1">{job.company} • {job.location}</p>
       {job.workType && (
         <span className="mt-2 inline-block bg-blue-100 text-blue-700 text-xs font-semibold px-3 py-1 rounded-full">
